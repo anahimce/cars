60ca89ab218597684dd0cd51f6880ae155a761fb
anahimce <ana_himce@yahoo.com>
2019-02-08 22:00:07 +0100

    8 zadatak

--- cars/resources/views/cars/show.blade.php
+++ cars/resources/views/cars/show.blade.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>{{$car->title}}</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('title')
+{{ $car->title }}
+@endsection
+
+
+@section('content')
     <div>
         <h1>{{ $car->producer }}</h1>
         <h2>{{ $car->title }}</h2>    
-        </div>
-</body>
-</html>
+    </div>
+@endsection
