7c59954ef4576a4a5600ca68fc05227735600bf3
anahimce <ana_himce@yahoo.com>
2019-02-08 21:28:20 +0100

    7.zadatak

--- cars/app/Http/Controllers/CarsController.php
+++ cars/app/Http/Controllers/CarsController.php
@@ -45,9 +45,9 @@ class CarsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Car $car)
     {
-        //
+        return view('cars.show', compact('car'));
     }
 
     /**
